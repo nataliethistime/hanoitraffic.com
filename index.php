f1ed13c9dde8ea5dca627a3799a6bf17adeaa1ba
Nathan McCallum <hey@nathan-mccallum.com>
2021-10-08 19:41:02 +1000

    Played at the bearded lady

--- index.php
+++ index.php
@@ -99,6 +99,7 @@
       <div class="container">
         <h1 class="title is-size-3">past shows</h1>
         <ul>
+          <li>07/09/2021 @ the bearded lady, west end</li>
           <li>11/09/2021 @ common house, fortitude valley</li>
           <li>03/09/2021 @ the cave inn, woolloongabba</li>
           <li>02/09/2021 @ the cave inn, woolloongabba</li>
